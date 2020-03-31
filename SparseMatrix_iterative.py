6cdeb08653bc3061cfd499f7ea68badc10740f8c
Alexandre MARIN <alexandre.marin1@laposte.net>
2020-03-31 15:13:45 +0200

    I have added some comments.

--- SparseMatrix_iterative.py
+++ SparseMatrix_iterative.py
@@ -26,7 +26,7 @@ def g(x,y) :
 comm = MPI.COMM_WORLD.Dup()
 # Interrogation du contexte MPI :
 rank = comm.rank
-nbDoms = comm.size
+nbDoms = comm.size #nombre de domaines
 
 # Ouverture d'un fichier nom unique mode simple
 fileName = "sortie{}.txt".format(rank)
@@ -35,9 +35,10 @@ file = open(fileName, mode="w")
 file.write("Rang du processus : {}\n".format(rank))
 file.write("Nombre de processus : {}\n".format(nbDoms))
 
-########################
+
+#Process 0 reads the mesh, builds data structures and send them to processes 1,...,nbDoms-1
 if rank == 0:
-    m = mesh.read("CarrePetit.msh")
+    m = mesh.read("CarreGros.msh")
     coords    = m[0]
     elt2verts = m[1]
     nbVerts = coords.shape[0]
@@ -58,7 +59,7 @@ if rank == 0:
             elt2doms[e] = ia
         ia += 1
 
-    # Calcul l'interface :
+    # Calcule l'interface :
     ie = 0
     mask = np.array([-1,]*nbVerts, np.short)
     for e in elt2verts :
@@ -91,11 +92,12 @@ if rank == 0:
 
     VSM.view( coords, elt2verts, nbDoms, elt2doms, indInterfNodes = interfNodes, title='Partition par elements')
     comm.bcast((nbVerts, nbElts), root = 0)
-    print(etsDomains[0].shape)
     comm.Bcast(coords, root = 0)
     comm.Bcast(elt2verts, root = 0)
     ets_loc2glo = comm.scatter(etsDomains, root = 0)
+    #ets_loc2glo associates indices of local elements with indices of elements in the global mesh
 else:
+    #we receive information
     nbVerts, nbElts = comm.bcast(None, root = 0)
     coords = np.empty((nbVerts, 4))
     comm.Bcast(coords, root = 0)
@@ -103,8 +105,10 @@ else:
     comm.Bcast(elt2verts, root = 0)
     ets_loc2glo = comm.scatter(None, root = 0)
 
-#local mesh
-print(rank, ets_loc2glo)
+
+#we build here the local mesh:
+
+#e2v_loc : equivalent to elt2verts but for the local mesh
 e2v_loc = np.empty((ets_loc2glo.shape[0], 3), dtype=np.int64)
 v_loc = set()
 for ie in ets_loc2glo:
@@ -112,13 +116,13 @@ for ie in ets_loc2glo:
     v_loc.add(elt2verts[ie, 1])
     v_loc.add(elt2verts[ie, 2])
 
-v_loc2glo = sorted(v_loc)
-v_glo2loc = np.array([-1]*coords.shape[0])
+v_loc2glo = sorted(v_loc)#associates local indices of vertices to global indices of vertices
+v_glo2loc = np.array([-1]*coords.shape[0])#this is the inverse map of v_loc2glo
 
 for ind in range(len(v_loc2glo)):
     v_glo2loc[v_loc2glo[ind]] = ind
 
-
+#we give values for e2v_loc
 for ie_loc in range(len(ets_loc2glo)):
     ie = ets_loc2glo[ie_loc]
     e2v_loc[ie_loc, 0] = v_glo2loc[elt2verts[ie, 0]]
@@ -126,17 +130,20 @@ for ie_loc in range(len(ets_loc2glo)):
     e2v_loc[ie_loc, 2] = v_glo2loc[elt2verts[ie, 2]]
 
 
+#structure for the sparse local matrix
 begVert2Elts_loc, vert2elts_loc = mesh.compvert2elts(e2v_loc)
 
 begRows_loc, indCols_loc = fem.comp_skel_csr_mat(e2v_loc, (begVert2Elts_loc, vert2elts_loc) )
 nz = begRows_loc[-1]
-print("Number of non zero in sparse matrix : {}".format(nz))
+file.write("\nNumber of non zero in sparse matrix : {}".format(nz))
 
+#number of vertices and number of elements in the local mesh
 nbV_loc = len(v_loc2glo)
 nbE_loc = e2v_loc.shape[0]
 
-spCoefs = np.zeros( (nz,), np.double)
+spCoefs = np.zeros( (nz,), np.double)#coefficients of the sparse matrix
 
+#we add elementary matrices, only for each element in the local mesh
 for iElt in range(nbE_loc):
     iVertices = e2v_loc[iElt,:]
     crd1 = coords[v_loc2glo[iVertices[0]],:]
@@ -159,16 +166,17 @@ for i in range(nbV_loc) :
     for ptR in range(begRows_loc[i],begRows_loc[i+1]):
         b[i] -= spCoefs[ptR]*f[indCols_loc[ptR]]
 
-bdry = set()
-#file.write("coords[:,3]\n")
+
+#we compute 'bdry', intersection of the boundary of the global mesh and of the boundary of the local mesh
+bdry = set() #it will contain global indices
+
 for iV in range(nbV_loc):
     iVglo = v_loc2glo[iV]
-    #file.write(str(iVglo)+", "+str(coords[iVglo, 3])+"\n")
     if coords[iVglo, 3] > 0:
         bdry.add(iVglo)
 
-#interface
-edges = set()
+#we compute the interface (with other local meshes)
+edges = set()#boundary of the local mesh, global indices are considered
 for ie in range(nbE_loc):
     ieglo = ets_loc2glo[ie]
     for i in range(3):
@@ -184,14 +192,16 @@ for ie in range(nbE_loc):
         else:
             edges.add(edge)
 
-interfNodes = set()
+interfNodes = set() #this variable will contain nodes at the interface of the local mesh (except those in the boundary of the global mesh)
 for e in edges:
     interfNodes.add(e[0])
     interfNodes.add(e[1])
 
-interfNodes = interfNodes - bdry
+interfNodes = interfNodes - bdry #we process differently nodes of the interface which are also in the boundary of the global mesh
+
+#we ensure each node in the interface will be considered by only one process (for the scalar product function, see below)
 interfNodesToDiscard = set()
-file.write("interfNodes:\n"+str(interfNodes)+"\n")
+file.write("\n\ninterfNodes:\n"+str(interfNodes)+"\n")
 for ip in range(rank):
     interfNodes2 = comm.recv(source=ip)
     interfNodesToDiscard = interfNodesToDiscard | (interfNodes2 & interfNodes)
@@ -200,13 +210,23 @@ for ip in range(rank+1, nbDoms):
     comm.send(interfNodes, dest=ip)
 
 
-
+#now for the boundary conditions, nodes in the boundary AND in the interfaces must be associated with only one process
 #we can do better by defining more comms
-toNullify = set()
+toNullify = set() #it will contain global indices of nodes in the local interface AND in the global boundary
+#thus, the matching line in the global matrix will be correct
+
+#too complex
+#subcomm = []
+#for ip in range(rank+1):
+#    subcomm.append(comm.Split(color=ip, key=rank))
+#for ip in range(rank+1, nbDoms):
+#    comm.Split(color=MPI.UNDEFINED, key=rank)
+
+#that method works:
 for ip in range(rank):
     bdry2 = comm.recv(source=ip)
     toNullify = toNullify | (bdry & bdry2)
-file.write("toNullify:\n"+str(toNullify)+"\n")#"to suppress"
+file.write("toNullify:\n"+str(toNullify)+"\n")
 for ip in range(rank+1, nbDoms):
     comm.send(bdry, dest=ip)
 
@@ -214,54 +234,50 @@ for ip in range(rank+1, nbDoms):
 # Il faut maintenant tenir compte des conditions limites :
 for iVert in bdry:
     iVloc = v_glo2loc[iVert]
-    if coords[iVert,3] > 0: # C'est une condition limite !########
-        # Suppression de la ligne avec 1 sur la diagonale :
-        for i in range(begRows_loc[iVloc],begRows_loc[iVloc+1]):
-            if indCols_loc[i] != iVloc or iVert in toNullify:
-                spCoefs[i] = 0.
-            else :
-                spCoefs[i] = 1.
-        # Suppression des coefficients se trouvant sur la colonne iVert :
-        for iRow in range(nbV_loc):
-            if iRow != iVloc :
-                for ptCol in range(begRows_loc[iRow],begRows_loc[iRow+1]):
-                    if indCols_loc[ptCol] == iVloc :
-                        spCoefs[ptCol] = 0.
+    # Suppression de la ligne avec 1 sur la diagonale :
+    for i in range(begRows_loc[iVloc],begRows_loc[iVloc+1]):
+        if indCols_loc[i] != iVloc or iVert in toNullify:
+            spCoefs[i] = 0.
+        else :
+            spCoefs[i] = 1.
+    # Suppression des coefficients se trouvant sur la colonne iVert :
+    for iRow in range(nbV_loc):
+        if iRow != iVloc :
+            for ptCol in range(begRows_loc[iRow],begRows_loc[iRow+1]):
+                if indCols_loc[ptCol] == iVloc :
+                    spCoefs[ptCol] = 0.
                         
-        b[iVloc] = f[iVloc]
+    b[iVloc] = f[iVloc]
+
 # On definit ensuite la matrice :
 spMatrix = sparse.csc_matrix((spCoefs, indCols_loc, begRows_loc),
                              shape=(nbV_loc,nbV_loc))
-#file.write("Matrice creuse {}:\n{}\n\n".format(spMatrix.shape, spMatrix))
 
-file.write("b_loc:\n"+str(b)+"\n")
+#file.write("\nMatrice creuse {}:\n{}\n\n".format(spMatrix.shape, spMatrix))
+#file.write("\nb_loc:\n"+str(b)+"\n")
 
-#we build a part of global b
+#we build a part of global b (no problem due to overlapping)
 partOfbGlo = np.zeros(nbVerts)
 for iVloc in set(range(nbV_loc)):
     iVglo = v_loc2glo[iVloc]
     if iVglo not in toNullify:
         partOfbGlo[iVglo] = b[iVloc]
-print(partOfbGlo)
+
 partsOfb = comm.allgather(partOfbGlo)
-bGlo = np.zeros(nbVerts)
+bGlo = np.zeros(nbVerts) #will contain the global second member
 for pob in partsOfb:
     bGlo += pob
-print("bGlo:", bGlo)
-file.write("bGol:\n"+str(bGlo)+"\n")
+
+
 # Visualisation second membre :
 #VS.view( coords, elt2verts, b, title = "second membre" )
 
-# Résolution du problème séquentiel avec gradient conjugue :
-iteration = 1
-def display_iter(x_k):
-    global iteration
-    print("iteration {:03d} : ||A.xk-b||/||b|| = {}".format(iteration, np.linalg.norm(spMatrix * x_k - b)/np.linalg.norm(b)))
-    iteration += 1
-
-file.write("spMat_loc:\n"+str(spMatrix))
 
 def prodMV(x):
+    """
+    That function computes the product of the global matrix by the vector 'x'
+    Each process handles its local sparse matrix.
+    """
     x_loc = np.zeros(nbV_loc)
     for iVloc in range(nbV_loc):
         iVglo = v_loc2glo[iVloc]
@@ -280,14 +296,8 @@ def prodMV(x):
         partOfy += recvbuf[ip*nbVerts:(ip+1)*nbVerts]
     return partOfy
 
-x=np.zeros(nbVerts)
-x[0]=1
-x[8] = 0.6
-y= prodMV(x)
-print("e_i: ", y)
 
-file.write("matloc*b_loc:\n"+str(spMatrix.dot(b)))
-
+#we compute global indices of nodes to consider for the scalar product
 nodesToConsider = set()
 for iVloc in range(nbV_loc):
     iVglo = v_loc2glo[iVloc]
@@ -296,6 +306,10 @@ for iVloc in range(nbV_loc):
 nodesToConsider -= (interfNodesToDiscard | toNullify)
 
 def scalarProd(x, y):
+    """
+    That function computes the scalar product of 'x' by 'y'.
+    Each coordinate of 'x' and 'y' is associated with only one process.
+    """
     partOfRes = 0
     for iV in nodesToConsider:
         partOfRes += x[iV]*y[iV]
@@ -306,15 +320,13 @@ def scalarProd(x, y):
         partOfRes += partsOfRes[ip]
     return partOfRes
 
-print("e1^2:", str(scalarProd(x, x)))
 
+#resolution
 sol, info = solver_gc(None, bGlo, prodMatVect=prodMV, verbose=(rank==0), prodScal=scalarProd)
+
 if rank==0:
-    print("sol: ", sol)
+    #print("sol: ", sol)
     # Visualisation de la solution :
     VS.view( coords, elt2verts, sol, title = "Solution" )
-#d = spMatrix.dot(sol) - b
-#print("||A.x-b||/||b|| = {}".format(sqrt(d.dot(d)/b.dot(b))))
-# Visualisation de la solution :
-#VS.view( coords, elt2verts, sol, title = "Solution" )
+
     
